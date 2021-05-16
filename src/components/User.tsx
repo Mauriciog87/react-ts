6ef1045619a84d6c286d3d37027281eb579c6660
Mauricio <mauriciog87@gmail.com>
2021-05-16 03:54:39 -0300

    its too late

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,11 +1,35 @@
-import { useState } from "react"
+import { useState } from 'react';
+
+interface User {
+    uid: string;
+    name: string;
+}
 
 export const User = () => {
-    const [user, setUser] = useState();
+    const [user, setUser] = useState<User>();
+
+    const login = () => {
+        setUser({
+            uid: 'ABC123',
+            name: 'Mauricio'
+        });
+    }
 
     return (
-        <div>
+        <div className="mt-5">
+            <h3>User: useState</h3>
+            <button
+                className="btn btn-outline-primary"
+                onClick={login}
+            >
+                Login
+            </button>
 
+            {
+                (!user)
+                    ? <pre>User does not exist</pre>
+                    : <pre>{JSON.stringify(user)}</pre>
+            }
         </div>
     )
 }
